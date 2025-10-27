2e87d6607d05e6fba7d5bdc34289b08e2352c911
pucki1988 <andi.schuster17@googlemail.com>
2025-10-27 11:57:37 +0100

    Update with config

--- js/style.js
+++ js/style.js
@@ -11,7 +11,7 @@ function parseCSV(csv, delimiter = ';') {
     });
 }
 
-function createHTMLRow(kandidat) {
+function createHTMLRowCandidate(kandidat) {
     return `
     <div class="pure-u-1 pure-u-sm-1-2 pure-u-lg-1-3 pure-u-xl-1-4 l-box kandidat pb-2">
         <div class="kandidat-content">
@@ -29,11 +29,35 @@ function createHTMLRow(kandidat) {
     </div>`;
 }
 
+
+function createHTMLRowVorstand(vorstand) {
+    return `
+    <div class="pure-u-1 pure-u-sm-1-2 pure-u-lg-1-3 pure-u-xl-1-4 l-box kandidat pb-2">
+        <div class="kandidat-content">
+            <img src="images/wahl_2020/${decodeURIComponent(vorstand.image)}" alt="${vorstand.name}"/>
+            <div class="kandidat-info">
+                <div class="kandidat-info-inner">
+                    <h4>${vorstand.position}</h4><br>
+                    <span>${vorstand.name}</span><br>
+                </div>
+            </div>
+        </div>
+    </div>`;
+}
+
 // Verarbeitung:
 fetch('config/kandidaten.csv')
   .then(res => res.text())
   .then(csvText => {
     const kandidaten = parseCSV(csvText);
-    document.getElementById('kandidaten').innerHTML =
-      kandidaten.map(createHTMLRow).join('');
+    //document.getElementById('kandidaten').innerHTML =
+    //kandidaten.map(createHTMLRowCandidate).join('');
+  });
+
+fetch('config/vorstand.csv')
+  .then(res => res.text())
+  .then(csvText => {
+    const vorstandschaft = parseCSV(csvText);
+    document.getElementById('vorstand').innerHTML =
+      vorstandschaft.map(createHTMLRowVorstand).join('');
   });
